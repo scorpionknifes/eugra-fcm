7165a774556f4cccb2366ea977a6e8923d1af176
Cheng-Zhen Yang <chengzhenyang@gmail.com>
2019-08-17 18:26:01 +1200

    added rest api support, grpc didn't work

--- fcm.go
+++ fcm.go
@@ -1,17 +1,58 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
+	"net/http"
 
 	"github.com/appleboy/go-fcm"
 )
 
-func fcmsend() {
+type Status struct {
+	Status  string `json:"status"`
+	Message string `json:"message"`
+}
 
+func SendFollowers(w http.ResponseWriter, r *http.Request) {
+	log.Print("./SendFollowers")
+	id, ok := r.URL.Query()["id"]
+	if !ok || len(id[0]) < 1 {
+		response := Status{
+			Status:  "400",
+			Message: "Url Param 'id' is missing",
+		}
+		json.NewEncoder(w).Encode(response)
+		return
+	}
+
+	var display_name string
+	var topic_id int64
+	var table_master_id int64
+	var topic_name string
+
+	row := SQL.QueryRow("SELECT users.id, users.name, tables.topic_id FROM eugra_users as users INNER JOIN eugra_tables as tables ON users.id = tables.table_master_id WHERE tables.id =?", id[0])
+	row.Scan(&table_master_id, &display_name, &topic_id)
+	row = SQL.QueryRow("SELECT `title` FROM `eugra_topics` WHERE `id`=?", topic_id)
+	row.Scan(&topic_name)
+	rows, _ := SQL.Query("SELECT `follower_id` FROM `eugra_follows` WHERE `user_id`=?", table_master_id)
+	for rows.Next() {
+		var followerid int64
+		rows.Scan(&followerid)
+		log.Println(followerid)
+		rows, _ := SQL.Query("SELECT `token` FROM `eugra_firebase` WHERE `user_id`=?", followerid)
+		for rows.Next() {
+			var token string
+			rows.Scan(&token)
+			fcmMessage(token, display_name, topic_name)
+		}
+	}
+}
+
+func fcmMessage(token string, display_name string, topic_name string) {
 	// Create the message to be sent.
 	msg := &fcm.Message{
-		To:           "dDuANNNc6YY:APA91bF82k21H-TNuDa-IlAY56EWmxJeH987RFGHAoxxZO2OTBObfnCiarwU6ZtrgPt0p7VTKUDIT1Ts0gbOdtSFOZPadRDLvLgvkgB49RltTvaXRddnNAHuUHd5adBQxMLNahgUcaA7",
-		Notification: &fcm.Notification{Title: "JIMMY" + " has Grab a new Table", Body: "TOPIC TITLE"},
+		To:           token,
+		Notification: &fcm.Notification{Title: display_name + " has Grab a new Table", Body: topic_name},
 		Data: map[string]interface{}{
 			"table_id": 10,
 		},
@@ -28,6 +69,5 @@ func fcmsend() {
 	if err != nil {
 		log.Fatalln(err)
 	}
-
 	log.Printf("%#v\n", response)
 }
--- main.go
+++ main.go
@@ -2,35 +2,81 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"log"
-	"net"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
-	"google.golang.org/grpc"
+	_ "github.com/go-sql-driver/mysql"
+
+	"github.com/gorilla/mux"
 )
 
-const (
-	port = ":50051"
-)
+const local string = "LOCAL"
 
-// server is used to implement helloworld.GreeterServer.
-type server struct{}
+//SQL & MQTT settings
+var SQL *sql.DB
 
-// SayHello implements helloworld.GreeterServer
-func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
-	log.Printf("Received: %v", in.Name)
-	return &pb.HelloReply{Message: "Hello " + in.Name}, nil
+//Function to Start APP
+func main() {
+	var (
+		// environment variables
+		env        = os.Getenv("ENV")  // LOCAL, DEV, STG, PRD
+		port       = os.Getenv("PORT") // server traffic on this port
+		connection = os.Getenv("SQL")
+	)
+	if env == "" || env == local {
+		env = local
+		port = "3004"
+		connection = "eugra:eugra@tcp(114.23.220.120:3306)/eugra-api?parseTime=true"
+	}
+	//Initialise Database Connection
+	var errs error
+	SQL, errs = sql.Open("mysql", connection)
+	if errs != nil {
+		log.Fatal(errs)
+	}
+	defer SQL.Close()
+
+	StartAPI(port)
 }
 
-// main start a gRPC server and waits for connection
-func main() {
-	lis, err := net.Listen("tcp", port)
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
-	}
-	s := grpc.NewServer()
-	pb.RegisterGreeterServer(s, &server{})
-	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
-	}
+//Start Simple REST API
+func StartAPI(port string) {
+	router := mux.NewRouter()
 
+	//Handlers for REST API
+	router.HandleFunc("/", SendFollowers)
+
+	srv := &http.Server{
+		Addr:    ":" + port, //Connection Port
+		Handler: router,
+	}
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	//Check REST API Connection
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+	log.Print("Server Started")
+
+	//Function to Stop Server
+	<-done
+	log.Print("Server Stopped")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer func() {
+		// extra handling here
+		cancel()
+	}()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatalf("Server Shutdown Failed:%+v", err)
+	}
+	log.Print("Server Exited Properly")
 }
